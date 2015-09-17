b95d120113040aad5f78e7371d61b25c47a5dc62
LegendKrazy <kdaniti@gmail.com>
2015-09-16 21:58:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 "use strict";
+
+var worker = require('worker');
+
 let spawn = Game.spawns.Spawn1;
 let sources = spawn.room.find(FIND_SOURCES);
-let workerCount = 3;
+let workerCount = 5;
+       
 
 const MINING_ACTION = 1;
 const COURIER_ACTION = 2;
@@ -25,37 +29,10 @@ let spawnWorker = function() {
     };
 };
 
-let doThing=function (index, worker) {
-    worker.memory.action = worker.memory.action || MINING_ACTION;
-    var targets = worker.room.find(FIND_CONSTRUCTION_SITES);
-
-    if (worker.memory.action === MINING_ACTION) {
-	worker.moveTo(sources[index % 2]);
-	worker.harvest(sources[index % 2]);
-	if (worker.carry.energy === worker.carryCapacity) {
-	    worker.memory.action = COURIER_ACTION;
-	}
-    } else {
-	if (spawn.energy < spawn.energyCapacity) {
-	    worker.moveTo(spawn);
-	    worker.transferEnergy(spawn);
-	} else if(targets.length) {
-	    worker.moveTo(targets[0]);
-	    worker.build(targets[0]);
-	}
-	else {
-	    worker.moveTo(spawn.room.controller);
-	    worker.upgradeController(spawn.room.controller);
-	}
-	if (worker.carry.energy === 0) {
-	    worker.memory.action = MINING_ACTION;
-	}
-    }
-};
 for (let index in workerList) {
     let workerName = workerList[index];
     let worker = Game.creeps[workerName];
-    doThing(index, worker);
+    worker(worker, index);
 }
 
 
